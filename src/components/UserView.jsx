74414851de3d53ab65f28dfb49fb067c207de567
LeoDV1901 <157930536+LeoDV1901@users.noreply.github.com>
2025-04-12 02:08:40 -0600

    Update UserView.jsx

--- src/components/UserView.jsx
+++ src/components/UserView.jsx
@@ -1,25 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import './css/UsersList.css'
+import './css/UsersList.css';
 
 const UserDetails = () => {
-  const { id } = useParams(); // Obtener el id desde la URL
+  const { id } = useParams();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        const response = await axios.get(`https://18.188.32.86/users/usuarios`, {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem('authToken')}`,
-          },
-        });
+        const response = await axios.get(`https://18.188.32.86/users/usuario/${id}`);
         setUser(response.data);
         setLoading(false);
       } catch (error) {
-        console.error('Error fetching user details', error);
+        console.error('Error al obtener detalles del usuario:', error);
         setLoading(false);
       }
     };
@@ -27,20 +23,14 @@ const UserDetails = () => {
     fetchUser();
   }, [id]);
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (!user) {
-    return <div>User not found</div>;
-  }
+  if (loading) return <div>Cargando...</div>;
+  if (!user) return <div>Usuario no encontrado</div>;
 
   return (
     <div className="user-details-container">
       <h2>Detalles del Usuario</h2>
       <p><strong>Nombre:</strong> {user.name}</p>
       <p><strong>Email:</strong> {user.email}</p>
-      {/* Mostrar más detalles según sea necesario */}
     </div>
   );
 };
